d9a0ffc921f14b91d8f7af6a0c1de6f88393c086
Mike Warren <warren.michael.douglas@gmail.com>
2022-10-18 16:58:37 -0500

    generated models, added Employee controller & test method

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeAssetManager.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OfficeAssetManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly OfficeAssetManagementContext _context;
+
+        public EmployeeController(ILogger<WeatherForecastController> logger, OfficeAssetManagementContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // GET: api/Employee/{employeeId}
+        [HttpGet("{employeeId}")]
+        public IEnumerable<Employee> Get(int employeeId)
+        {
+            var query = from Employee in _context.Employees
+                        where Employee.EmployeeId == employeeId
+                        select Employee;
+
+            IEnumerable<Employee> result = query;
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OfficeAssetManager.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true).Build();
+
+//var options = new DbContextOptionsBuilder<OfficeAssetManagementContext>().UseSqlServer(config["ConnectionString"]).Options;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<OfficeAssetManagementContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("OfficeAssetManagementContext")));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
