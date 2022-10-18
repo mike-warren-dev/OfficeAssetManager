a4ae21b4f0d4a88166657cc51a8b9d9bfc5b993e
Mike Warren <lajksdgbiu@gmail.com>
2022-10-18 15:36:10 -0500

    Added models

---
+++ Models/Asset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeAssetManager.Models
+{
+    public partial class Asset
+    {
+        public Asset()
+        {
+            AssetAssignments = new HashSet<AssetAssignment>();
+        }
+
+        public int AssetId { get; set; }
+        public Guid? Guid { get; set; }
+        public int? AssetTypeId { get; set; }
+        public int? AssetStatusId { get; set; }
+        public string? AddedBy { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public string? RemovedBy { get; set; }
+        public DateTime? RemovedDate { get; set; }
+
+        public virtual ICollection<AssetAssignment> AssetAssignments { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeAssetManager.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            AssetAssignments = new HashSet<AssetAssignment>();
+        }
+
+        public int EmployeeId { get; set; }
+        public int? ExternalSystemId { get; set; }
+        public string DisplayName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int? SiteId { get; set; }
+
+        public virtual ICollection<AssetAssignment> AssetAssignments { get; set; }
+    }
+}
