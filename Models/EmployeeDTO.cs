e8704d8690171eb553519b0e6f1cc491e87db91b
Mike Warren <lajksdgbiu@gmail.com>
2022-10-19 13:42:37 -0500

    moved ee assets to a different endpoint

---
+++ Models/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OfficeAssetManager.Models
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeId { get; set; }
+        public int? ExternalSystemId { get; set; }
+        public string DisplayName { get; set; } = null!;
+        public string? SiteName { get; set; }
+    }
+}
