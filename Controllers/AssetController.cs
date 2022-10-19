30d0036ae1aac7b60a2c7142334063d36919e079
Mike Warren <warren.michael.douglas@gmail.com>
2022-10-19 16:57:21 -0500

    Added asset controller, rejiggered DTOs

---
+++ Controllers/AssetController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using OfficeAssetManager.Models;
+
+using static System.Guid;
+
+namespace OfficeAssetManager.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AssetController : ControllerBase
+{
+    private readonly OfficeAssetManagementContext _context;
+
+    public AssetController(OfficeAssetManagementContext context)
+    {
+        _context = context;
+    }
+
+    //GET: api/Asset/{guid}
+    [HttpGet("{guid}")]
+    public IActionResult Get(Guid guid)
+    {
+
+        var result = (from a in _context.Assets
+                      where a.Guid == guid
+                      join dv in _context.DictionaryValues
+                          on a.AssetTypeId equals dv.ValueId
+                      join map in _context.AssetAssignments
+                          on a.AssetId equals map.AssetId
+                      join ee in _context.Employees
+                          on map.EmployeeId equals ee.EmployeeId
+                      select new AssetDTO
+                      {
+                          Guid = guid,
+                          AssetTypeDisplayName = dv.DisplayName,
+                          AssignedTo = ee.DisplayName
+                      }).FirstOrDefault();
+
+        return (result == null) ? NoContent() : Ok(result);
+    }
+}
--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,39 +3,38 @@ using OfficeAssetManager.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace OfficeAssetManager.Controllers
+namespace OfficeAssetManager.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
+    private readonly OfficeAssetManagementContext _context;
+
+    public EmployeeController(OfficeAssetManagementContext context)
     {
-        private readonly OfficeAssetManagementContext _context;
+        _context = context;
+    }
 
-        public EmployeeController(OfficeAssetManagementContext context)
-        {
-            _context = context;
-        }
+    // GET: api/Employee/{employeeId}
+    [HttpGet("{employeeId}")]
+    public IActionResult Get(int employeeId)
+    {
+        var query = from ee in _context.Employees
+                    where ee.EmployeeId == employeeId
+                    join dv in _context.DictionaryValues
+                        on ee.SiteId equals dv.ValueId
+                    select new EmployeeDTO
+                    {
+                        EmployeeId = ee.EmployeeId,
+                        ExternalSystemId = ee.ExternalSystemId,
+                        DisplayName = ee.DisplayName,
+                        SiteName = dv.DisplayName
+                    };
 
-        // GET: api/Employee/{employeeId}
-        [HttpGet("{employeeId}")]
-        public IActionResult Get(int employeeId)
-        {
-            var query = from ee in _context.Employees
-                        where ee.EmployeeId == employeeId
-                        join dv in _context.DictionaryValues
-                            on ee.SiteId equals dv.ValueId
-                        select new EmployeeDTO
-                        {
-                            EmployeeId = ee.EmployeeId,
-                            ExternalSystemId = ee.ExternalSystemId,
-                            DisplayName = ee.DisplayName,
-                            SiteName = dv.DisplayName
-                        };
-
-            IEnumerable<EmployeeDTO> result = query;
+        IEnumerable<EmployeeDTO> result = query;
 
 
-            return (query.Any()) ? Ok(result) : NoContent();
-        }
+        return (query.Any()) ? Ok(result) : NoContent();
     }
 }
