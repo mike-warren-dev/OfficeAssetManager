ea3febc5912e31687d091304b6776cdd903c1a2a
Mike Warren <warren.michael.douglas@gmail.com>
2022-10-21 11:39:19 -0500

    added delete endpoint

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace OfficeAssetManager.Models
 {
@@ -9,11 +8,7 @@ namespace OfficeAssetManager.Models
         public int AssetId { get; set; }
         public Guid? Guid { get; set; }
         public int? EmployeeId { get; set; }
-        
-        [Range(50, 149, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int? AssetTypeId { get; set; }
-        
-        [Range(149, 199, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int? AssetStatusId { get; set; }
         public string? AddedBy { get; set; }
         public DateTime? AddedDate { get; set; }
--- Repositories/IEmployeeRepository.cs
+++ Repositories/IEmployeeRepository.cs
@@ -5,6 +5,7 @@ namespace OfficeAssetManager.Repositories;
 public interface IEmployeeRepository 
 {
     IEnumerable<Employee> GetEmployees();
+    bool HasAssets(int employeeId);
     Employee? GetEmployeeById(int employeeId);
     void InsertEmployee(Employee employee);
     void UpdateEmployee(Employee employee);
