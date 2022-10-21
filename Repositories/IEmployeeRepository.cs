5aeadb4efbfbd07b395db1bb55a2f1e79d6b3edc
Mike Warren <warren.michael.douglas@gmail.com>
2022-10-21 11:45:25 -0500

    added delete endpoint, con't

--- Repositories/IEmployeeRepository.cs
+++ Repositories/IEmployeeRepository.cs
@@ -5,7 +5,6 @@ namespace OfficeAssetManager.Repositories;
 public interface IEmployeeRepository 
 {
     IEnumerable<Employee> GetEmployees();
-    bool HasAssets(int employeeId);
     Employee? GetEmployeeById(int employeeId);
     void InsertEmployee(Employee employee);
     void UpdateEmployee(Employee employee);
